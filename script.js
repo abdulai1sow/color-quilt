f61e6faea1df6c119ddbdf80c2e8a3527ade8567
sowabdulai <abdulai-as@hotmail.com>
2022-09-13 09:56:11 -0400

    colors first

---
+++ script.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body')
+
+const createSquare = (el) => {
+  for (let i = 1; i <= el; i++) {
+    // Create Element
+    const div = document.createElement("div");
+    div.textContent = i; // the index of each square
+    div.classList.add('square')//adding square class
+    // const body = document.querySelector("body"); 
+    body.appendChild(div);
+    colors(div)
+  }
+}
+
+const colors = (sqColor) => {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let rgColors = "rgb(" + red + "," + green + "," + blue + ")";
+  console.log(colors);
+
+  sqColor.style.background = rgColors;
+
+}
+
+createSquare(500)
+
